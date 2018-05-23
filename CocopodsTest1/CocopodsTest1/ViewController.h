1bf7e5b493f71dbdc84253eec3023e8c2e25c77a
dingmian <dingmian@ayibang.com>
2018-05-23 11:08:14 +0800

    我的

---
+++ CocopodsTest1/CocopodsTest1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocopodsTest1
+//
+//  Created by ayb on 2018/5/23.
+//  Copyright © 2018年 testDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CocopodsTest1/CocopodsTest1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CocopodsTest1
+//
+//  Created by ayb on 2018/5/23.
+//  Copyright © 2018年 testDemo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
